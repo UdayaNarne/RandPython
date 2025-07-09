7a51caf8c8c75518868f26c8134607a9c3694114
UdayaNarne <udayanarne08@gmail.com>
2025-07-09 15:30:56 +0530

    Added

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ def sum(a,b):
 def subtract(a,b):
     return a - b
 
+def multiply(a,b):
+    return a * b
+
+def divide(a,b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a / b
+
+
 print(sum(1,-2))
 print(subtract(1,-2))
 
