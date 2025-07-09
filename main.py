83e1d3bf14c130185609605fa78ea0f61c61ca91
UdayaNarne <udayanarne08@gmail.com>
2025-07-09 16:34:08 +0530

    some chnages

--- main.py
+++ main.py
@@ -7,10 +7,6 @@ def subtract(a,b):
 def multiply(a,b):
     return a*b
 
-def divide(a,b):
-    if b == 0:
-        raise ValueError("Cannot divide by zero")
-    return a / b
 
 print(sum(1,-2))
 print(subtract(1,-2))
