7c5c6af2188f11e153bf2e5e251bd1699f64fd8e
UdayaNarne <udayanarne08@gmail.com>
2025-07-10 07:14:25 +0530

    Made some changes

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
 def sum(a,b):
     return a + b
 
-def subtract(a,b):
-    return a - b
-
-def multiply(a,b):
-    return a*b
-
-print("Hello, World!!")
-
 print(sum(1,-2))
-print(subtract(1,-2))
 print("Hello Udaya Narne")
